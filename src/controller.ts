692230d1e9583f75e12404121bf268f272b3ceeb
vanyadelova <happy.vanya.delova@gmail.com>
2018-11-24 14:26:48 +0100

    First commit

---
+++ src/controller.ts
@@ -0,0 +1,14 @@
+
+import {Controller, Get} from 'routing-controllers'
+
+@Controller()
+export default class MainController {
+
+    @Get("/hello")
+    main() {
+       return {
+         hello: 'World'
+       }
+    }
+
+}
